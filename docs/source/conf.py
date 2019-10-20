066ace6820175da1e4621b3937e0cb336ce839dc
Audrey <tavares.audrey@gmail.com>
2019-10-20 12:32:38 -0400

    modified conf.py file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -92,7 +92,10 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_css_files = ['theme_override.css']
+
+def setup(app):
+    app.add_javascript("custom.js")
+    app.add_stylesheet("theme_override.css")
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
